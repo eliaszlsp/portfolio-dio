84f636439786f2d7a5d345d6fcdcaf19c9eba8d0
eliaszlsp <estudoselias@hotmail.com>
2023-07-20 21:36:25 -0300

    copy

---
+++ assets/js/api.js
@@ -0,0 +1,9 @@
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    console.log(profileData);
+    return profileData
+    
+}
